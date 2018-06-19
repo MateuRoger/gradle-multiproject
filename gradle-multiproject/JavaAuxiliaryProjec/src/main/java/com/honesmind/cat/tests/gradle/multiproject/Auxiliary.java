9f620c2f5054c724fc160165d168ee5c4772c46a
MateuRoger <mateu.roger@gmail.com>
2018-06-20 00:04:28 +0200

    java auxiliary project added

---
+++ gradle-multiproject/JavaAuxiliaryProjec/src/main/java/com/honesmind/cat/tests/gradle/multiproject/Auxiliary.java
@@ -0,0 +1,8 @@
+package com.honesmind.cat.tests.gradle.multiproject;
+
+public class Auxiliary {
+
+    protected static String getHelloWorld(){
+        return "Hello World";
+    }
+}
--- gradle-multiproject/MainJavaProject/src/main/java/com/honesmind/cat/tests/gradle/multiproject/HelloWorld.java
+++ gradle-multiproject/MainJavaProject/src/main/java/com/honesmind/cat/tests/gradle/multiproject/HelloWorld.java
@@ -2,6 +2,6 @@ package com.honesmind.cat.tests.gradle.multiproject;
 
 public class HelloWorld {
     public static void main(String[] args){
-        System.out.println("Hello World");
+        System.out.println(Auxiliary.getHelloWorld());
     }
 }
